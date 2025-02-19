d8f87eb5f6abbc7c5220fe7a8fc4ef3d97f3e93b
Ziad Eid <ziadeid.web@gmail.com>
2025-02-19 14:56:40 +0200

    wraping app with global context

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GlobalProvider } from './Context/GlobalContext'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <GlobalProvider>
+    <App />
+  </GlobalProvider>
 )
+
+// npm i axios react-router-dom formik yup bootstrap sweetalert2
